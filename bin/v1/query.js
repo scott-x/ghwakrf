9e0fd0118125a04470724db4e982a45f2f5844b9
scottxiong <github.scott.bear@gmail.com>
2019-03-19 16:03:47 +0800

    ss

--- bin/v1/query.js
+++ bin/v1/query.js
@@ -28,54 +28,87 @@ var jobDetailSchema = new Schema({
 var JobDetailModel = mongoose.model('benchmark_job_detail', jobDetailSchema); //创建blog数据库
 mongoose.connect('mongodb://localhost:27017/benchmark_job_detail',{ useNewUrlParser: true } );
 
+function runDb(obj){
+	  JobDetailModel.find(obj,(err,docs)=>{
+	  	if (err) {console.log('  Warning: 非法输入')}
+	    var n=0;
+	  	docs.forEach(data=>{
+	  		  n++;
+	  		  var additional_info = data.title.split("任     务     单")[1];
+	  		  if (additional_info) {
+	  		  	console.log(`${chalk.green("任     务     单")}${chalk.red(additional_info)}`)
+	  		  }else{
+	  		  	console.log(`${chalk.green(data.title)}`)
+	  		  }
+	        console.log(`${chalk.green(data.job)}`)
+	        console.log(`${chalk.green(data.brand_country)}`)
+	        console.log(`${chalk.green(data.author)}`)
+	        console.log(`${chalk.green(data.text)}`)
+	        if (data.additional_notes) {
+	          console.log(`${chalk.bold.red(data.additional_notes)}`)
+	        }
+	        console.log(`${chalk.green(data.create_time)}`)
+	        console.log(`${chalk.blue(data.program)}`)
+	        console.log(`${chalk.blue(data.supplier)}`)
+	        console.log(`${chalk.blue(data.buyer)}`)
+	        console.log(`${chalk.blue(data.due_date)}`)
+	        console.log(`${chalk.blue(data.packout_date)}`)
+	        console.log(`${chalk.blue(data.ship_date)}`)
+	        console.log(`${chalk.blue(data.instore_date)}`)
+	        console.log(`${chalk.blue(data.status)}`)
+	        
+	        console.log(`${chalk.green(data.contact)}`)
+	       
+	        console.log(`工单状态：${chalk.red.bold(data.job_status)}`)
+	        console.log("---------------------------------------------------------------")
+	  	})
+	  	console.log(`共有：${chalk.blue.bold(n)} 条数据`)
+	    mongoose.connection.close(); 
+	  })
+}
 module.exports.query=function(){
 	inquirer
       .prompt([
         /* Pass your questions in here */
+          
           {
-            type: 'input',
-            name: 'type',
-            message: `${chalk.magenta(' 请输入关键字(公司名或者单号里的某个数字,eg: gem 22):')}`
+            type: 'rawlist',
+            name: 'query',
+            message: `${chalk.magenta(' 选择你的查询方式:')}`,
+            choices: ['按工单号查询',"按系列,国家查询"],
+            default: '按工单号查询'
           }
        
       ])
 	  .then(answers => {
-	  	  JobDetailModel.find({job: {$regex: answers.type, $options: '$i'}},(err,docs)=>{
-	  	  	if (err) {console.log('  Warning: 非法输入')}
-	  	    var n=0;
-	  	  	docs.forEach(data=>{
-	  	  		  n++;
-	  	  		  var additional_info = data.title.split("任     务     单")[1];
-	  	  		  if (additional_info) {
-	  	  		  	console.log(`${chalk.green("任     务     单")}${chalk.red(additional_info)}`)
-	  	  		  }else{
-	  	  		  	console.log(`${chalk.green(data.title)}`)
-	  	  		  }
-	              console.log(`${chalk.green(data.job)}`)
-	              console.log(`${chalk.green(data.brand_country)}`)
-	              console.log(`${chalk.green(data.author)}`)
-	              console.log(`${chalk.green(data.text)}`)
-	              if (data.additional_notes) {
-	                console.log(`${chalk.bold.red(data.additional_notes)}`)
-	              }
-	              console.log(`${chalk.green(data.create_time)}`)
-	              console.log(`${chalk.blue(data.program)}`)
-	              console.log(`${chalk.blue(data.supplier)}`)
-	              console.log(`${chalk.blue(data.buyer)}`)
-	              console.log(`${chalk.blue(data.due_date)}`)
-	              console.log(`${chalk.blue(data.packout_date)}`)
-	              console.log(`${chalk.blue(data.ship_date)}`)
-	              console.log(`${chalk.blue(data.instore_date)}`)
-	              console.log(`${chalk.blue(data.status)}`)
-	              
-	              console.log(`${chalk.green(data.contact)}`)
-	             
-	              console.log(`工单状态：${chalk.red.bold(data.job_status)}`)
-	              console.log("---------------------------------------------------------------")
-	  	  	})
-	  	  	console.log(`共有：${chalk.blue.bold(n)} 条数据`)
-	  	    mongoose.connection.close(); 
-	  	  })
+	  	  let obj={};
+	  	  switch(answers.query){
+	  	  	case "按工单号查询":
+              inquirer.prompt([{
+	            type: 'input',
+	            name: 'type',
+	            message: `${chalk.magenta(' 请输入关键字(公司名或者单号里的某个数字,eg: gem 22):')}`
+              },])
+              .then(answers=>{
+                  // console.log("type",answers.type)
+                  obj={job: {$regex: answers.type, $options: '$i'}}
+                  runDb(obj)
+              })
+	  	  	  break;
+	  	  	case "按系列,国家查询": 
+	  	  	  
+	          inquirer.prompt([{
+		            type: 'input',
+		            name: 'type',
+		            message: `${chalk.magenta(' 请输入系列或国家(忽略大小写,如：hol,china):')}`
+	          },])
+	          .then(answers=>{
+	                // console.log("type",answers.type)
+                    obj={brand_country: {$regex: answers.type, $options: '$i'}}
+                    runDb(obj)
+	          })
+	  	  	  break; 
+	  	  }	  	  
 
 	  })           
 }
